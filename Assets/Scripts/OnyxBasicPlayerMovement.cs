caf3718c60e0db017641995358b79e91c529e6f8
Roman Garms <38168462+Romano-Garmez@users.noreply.github.com>
2023-11-05 17:30:09 -0800

    i can't believe you spelled "position" wrong.
    Also upped the movement speed

--- Assets/Scripts/OnyxBasicPlayerMovement.cs
+++ Assets/Scripts/OnyxBasicPlayerMovement.cs
@@ -6,89 +6,69 @@ public class OnyxBasicPlayerMovement : MonoBehaviour
 {
     public World world;
     public float moveSpeed = 10f;
-    public Vector3 realPosiiton;
+    public Vector3 realPosition;
     public GameObject virtualCamera;
     public float destinationProx = 1;
 
-
     // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    void Start() { }
 
     private void Update()
     {
-        Debug.Log(destinationProx + ", " + Vector3.Distance(transform.position, realPosiiton) );
-        
+        Debug.Log(destinationProx + ", " + Vector3.Distance(transform.position, realPosition));
 
         if (destinationProx < 1)
         {
             destinationProx += Time.deltaTime * moveSpeed;
             destinationProx = Mathf.Clamp(destinationProx, 0f, 1f);
-            transform.position = Vector3.Lerp(transform.position, realPosiiton, destinationProx);
+            transform.position = Vector3.Lerp(transform.position, realPosition, destinationProx);
         }
-            
+
         if (destinationProx == 1)
         {
             Vector3 direction = GetNearestHorizontalVector3(virtualCamera.transform);
             DoMovement(direction);
-            if (Vector3.Distance(transform.position, realPosiiton) > 0)
+            if (Vector3.Distance(transform.position, realPosition) > 0)
             {
                 destinationProx = 0f;
             }
         }
-
-
     }
 
     void DoMovement(Vector3 direction)
     {
-
         if (Input.GetKey(KeyCode.W))
         {
-            realPosiiton += direction;
-
+            realPosition += direction;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            realPosiiton += -direction;
-
+            realPosition += -direction;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-
-            realPosiiton += Quaternion.Euler(0, -90, 0) * direction;
-
+            realPosition += Quaternion.Euler(0, -90, 0) * direction;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-
-            realPosiiton += Quaternion.Euler(0, 90, 0) * direction;
-
+            realPosition += Quaternion.Euler(0, 90, 0) * direction;
         }
 
         if (Input.GetKey(KeyCode.LeftControl))
         {
-
-            realPosiiton += -Vector3.up;
-
+            realPosition += -Vector3.up;
         }
         if (Input.GetKey(KeyCode.Space))
         {
-            realPosiiton += Vector3.up;
+            realPosition += Vector3.up;
         }
-
-
-
     }
 
     Vector3 GetNearestHorizontalVector3(Transform transform)
     {
-
         float[] dirAngles = new float[4];
 
         dirAngles[0] = Vector3.Angle(transform.forward, Vector3.forward);
@@ -96,11 +76,13 @@ public class OnyxBasicPlayerMovement : MonoBehaviour
         dirAngles[2] = Vector3.Angle(transform.forward, Vector3.right);
         dirAngles[3] = Vector3.Angle(transform.forward, -Vector3.forward);
 
-
         int pos = 0;
         for (int i = 0; i < dirAngles.Length; i++)
         {
-            if (dirAngles[i] < dirAngles[pos]) { pos = i; }
+            if (dirAngles[i] < dirAngles[pos])
+            {
+                pos = i;
+            }
         }
         switch (pos)
         {
@@ -112,7 +94,8 @@ public class OnyxBasicPlayerMovement : MonoBehaviour
                 return Vector3.right;
             case 3:
                 return -Vector3.forward;
-            default: return Vector3.zero;
+            default:
+                return Vector3.zero;
         }
     }
 }
