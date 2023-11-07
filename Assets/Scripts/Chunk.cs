8e97dd976380079e98eb77dcebe2eb8c74f87cce
alterednode <75056546+alterednode@users.noreply.github.com>
2023-11-06 22:12:06 -0800

    Moved some Classes to their own file

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -368,32 +368,3 @@ public class Chunk
     }
 
 }
-
-public class ChunkCoord
-{
-    public int x;
-    public int y;
-    public int z;
-
-    public bool CompareCoord(ChunkCoord coord)
-    {
-        return (coord.x == x && coord.y == y && coord.z == z);
-    }
-
-    public override string ToString()
-    {
-        return $"{x}, {y}, {z}";
-    }
-    public ChunkCoord(int _x, int _y, int _z)
-    {
-        x = _x;
-        y = _y;
-        z = _z;
-
-
-    }
-    public ChunkCoord(int _x, int _z) : //TODO: learn why this is the right way, chatGPT spat this out :)
-        this(_x, 0, _z)
-    {
-    }
-}
